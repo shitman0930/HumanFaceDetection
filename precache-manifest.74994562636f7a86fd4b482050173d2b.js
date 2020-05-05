1fe604ffdaa32cef4298d78380915868702a9d5c
shitman0930 <63058942+shitman0930@users.noreply.github.com>
2020-05-05 12:05:43 +0800

    Updates

---
+++ precache-manifest.74994562636f7a86fd4b482050173d2b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b2f14788073c8a8f381ff5a2cd43355",
+    "url": "/face-detection/index.html"
+  },
+  {
+    "revision": "7d777b5c0e284e23443b",
+    "url": "/face-detection/static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "15b9d1cf5a9932bf47f3",
+    "url": "/face-detection/static/css/main.a2f9a7ad.chunk.css"
+  },
+  {
+    "revision": "7d777b5c0e284e23443b",
+    "url": "/face-detection/static/js/2.a14365f8.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/face-detection/static/js/2.a14365f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15b9d1cf5a9932bf47f3",
+    "url": "/face-detection/static/js/main.938d0321.chunk.js"
+  },
+  {
+    "revision": "3c174cc6803107e85689",
+    "url": "/face-detection/static/js/runtime-main.157e82b0.js"
+  }
+]);
